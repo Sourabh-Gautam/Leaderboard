136e0152edebd4b1f87b4e6f53abb6d45f32d571
Sourabh Gautam <Sourabh_Gautam@epam.com>
2023-05-04 12:19:37 +0530

    Descending Sorting added in dashboard hyberlinks

--- src/app/services/participant.service.ts
+++ src/app/services/participant.service.ts
@@ -20,39 +20,41 @@ export class ParticipantService {
   }
 
   async getParticipantByEmail(email: string, selectedYear: number) {
-    const response = await axios.get(`${this.pdBaseUrl}/email/${email}/${selectedYear}`);
-     this.participants = response.data.sort((a, b) => {
-     return b.awardedDate.localeCompare(a.awardedDate);
-     });
+    const response = await axios.get(
+      `${this.pdBaseUrl}/email/${email}/${selectedYear}`
+    );
+    this.participants = response.data.sort((a, b) => {
+      return b.awardedDate.localeCompare(a.awardedDate);
+    });
     return this.participants;
-    }
+  }
 
   async getParticipantByDesignation(value: string) {
     await axios
       .get(`${this.pdBaseUrl}/designation/${value}`)
       .then((response) => {
-        this.participants = response.data;
+        this.participants = response.data.sort((a, b) => b.points - a.points);
       });
     return this.participants;
   }
 
   async getParticipantByBU(value: string) {
     await axios.get(`${this.pdBaseUrl}/bu/${value}`).then((response) => {
-      this.participants = response.data;
+      this.participants = response.data.sort((a, b) => b.points - a.points);
     });
     return this.participants;
   }
 
   async getParticipantByPS(value: string) {
     await axios.get(`${this.pdBaseUrl}/ps/${value}`).then((response) => {
-      this.participants = response.data;
+      this.participants = response.data.sort((a, b) => b.points - a.points);
     });
     return this.participants;
   }
 
   async getParticipantByRM(value: string) {
     await axios.get(`${this.pdBaseUrl}/rm/${value}`).then((response) => {
-      this.participants = response.data;
+      this.participants = response.data.sort((a, b) => b.points - a.points);
     });
     return this.participants;
   }
